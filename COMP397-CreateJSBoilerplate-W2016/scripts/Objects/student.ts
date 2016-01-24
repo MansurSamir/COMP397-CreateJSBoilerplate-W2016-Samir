bedc9d61688331a7d7fc813c43228224b5fb17f7
MansurSamir <mansursamir777@gmail.com>
2016-01-24 16:22:24 -0500

    Initial Commit

---
+++ COMP397-CreateJSBoilerplate-W2016/scripts/Objects/student.ts
@@ -0,0 +1,16 @@
+﻿module objects {
+  export  class Student extends Person {
+        //INSTANT VARIABLES++++++++++++++++++++++++++++++++++++++++
+        private _studentNumber: number;
+        //CONSTRUCTOR++++++++++++++++++++++++++++++++++++++++++++++
+        constructor(name: string, studentNumber: number) {
+            super(name);
+            this._studentNumber = studentNumber;
+        }
+        //PUBLIC METHODS
+        public studies(): void {
+            console.log(this._name + " is studying");
+        }
+
+    }
+}
